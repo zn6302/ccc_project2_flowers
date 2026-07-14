2e64671fb13640e682f8f31dccbac12baa4c845e
114462018 <125728475+zn6302@users.noreply.github.com>
2026-07-14 10:39:17 +0800

    refactor: add centralized palettes

---
+++ js/palettes.js
@@ -0,0 +1,23 @@
+const FLOWER_PALETTE = [
+  "#7FA8C9",
+  "#A8C4D4",
+  "#C9DCE8",
+  "#DDE8ED",
+  "#F5F3ED",
+  "#EDEAE0",
+  "#6E93A8",
+  "#B8CBD8",
+  "#4A6C8C",
+  "#C9B8D8",
+  "#D9C9D4",
+  "#E8DCC8",
+];
+
+const STEM_PALETTE = [
+  "#8FA894",
+  "#7C9A82",
+  "#9BB09E",
+  "#A3B8A0",
+  "#6B8770",
+  "#8A9490",
+];
